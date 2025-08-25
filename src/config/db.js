fa987875f9aa8cee03cc9be8fc40acc08938787a
Sue-Holding <sue.holding55@gmail.com>
2025-08-25 10:04:07 +0200

    started init on influencer tracking project

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected at ${connect.connection.host}`);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
